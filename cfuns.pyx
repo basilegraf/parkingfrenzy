663de856a85e3605ca86501d6feae3b98a07e086
Basile <basile.graf@a3.epfl.ch>
2020-11-27 21:42:21 +0100

    speedup 30x

--- cfuns.pyx
+++ cfuns.pyx
@@ -1,5 +1,5 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
+# cython: language_level=3, boundscheck=False, overflowcheck=False, cdivision=True
+
 """
 Created on Fri Nov 27 16:22:24 2020
 
@@ -14,7 +14,7 @@ import array
 cdef binomial_c(int n, int k):
     cdef int b, t
     if not 0 <= k <= n:
-        return 0
+        return int(0)
     b = 1
     for t in range(min(k, n-k)):
         b *= n
@@ -29,14 +29,17 @@ cdef bell_c(int n, int k, double[:] x):
     """
     Evaluate the partial exponential Bell polynomial B_n,k(x)
     """
+    cdef int i
+    cdef double b, c
     if (n==0) & (k==0):
-        return 1
+        return 1.0
     elif (k==0) | (n==0):
-        return 0
+        return 0.0
     else:
-        b = 0
+        b = 0.0
         for i in range(n-k+1):
-            b += binomial_c(n-1, i) * x[i] * bell_c(n-i-1, k-1, x)
+            c = binomial_c(n-1, i) 
+            b += c * x[i] * bell_c(n-i-1, k-1, x)
         return b
     
 # Wrapper for binomial_c, should be used only for testing
