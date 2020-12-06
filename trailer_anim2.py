304c54675105b70a456f286e6758643fe284271a
Basile <basile.graf@a3.epfl.ch>
2020-12-06 13:39:03 +0100

    basic anim 2

--- trailer_anim2.py
+++ trailer_anim2.py
@@ -9,6 +9,7 @@ import math
 import trailer_poly as tp
 import scipy.interpolate as interp
 import numpy as np
+import numpy.matlib
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
@@ -23,40 +24,34 @@ except:
 
 n = 4
 m = 3
+p = n+m
+tMax = 1
 lx = 1
-ly = .3
-L = 0.3 * np.ones(n)
+ly = 1
+L = 1 * np.ones(n)
+tt=np.linspace(0,tMax,100)
 
-# n-th derivative spline coeff
-v = np.array([1.0])
-for k in range(n + m - 1):
-    v = np.concatenate((v,-v))
-v = np.concatenate(([0], v, [0]))
+# n+m-th  spline coeff
+beu = tMax * np.ones(n+m)
+t = np.concatenate((np.zeros(n+m), np.linspace(0,tMax,p+1), beu))
+c = np.concatenate((np.zeros(n+m), ly*np.ones(p), ly*np.ones(n+m)))
+
+b = interp.BSpline(t, c, n+m)
+
+plt.plot(tt, b.derivative(0)(tt))
+plt.grid(True)
     
 
-# c = v
-# for k in range(n+1):
-#     c = np.cumsum(c)
-#c = np.concatenate((np.zeros(n+1, dtype='int'), c))
 
-t = np.linspace(0, 1, len(v) + 1) # knots
-b = interp.BSpline(t,v,0)
-endVal = b.antiderivative(n + m)(1)
-b.c *= (ly / endVal)
 
-# v /= (t[1]-t[0])**(n) * math.factorial(n) * vv[-1]
 
-tt = np.linspace(0, 1, 10*len(v) + 1)
 
-b = interp.BSpline(t,v,0)
-bb=[b.antiderivative(m)]
-for k in range(1, n + 1):
-    bb.append(bb[-1].antiderivative(1))
+bb=[b]
+for k in range( n):
+    bb.append(bb[-1].derivative(1))
     #bb[-1].c *= max(n + 1 -k, 1)
     
-fac = ly / bb[-1].c[-1]
-for k in range(n):
-    bb[k].c *= fac
+
     
     
 if False:
@@ -67,7 +62,7 @@ if False:
     
 Y = np.zeros((len(bb), len(tt)))
 for k in range(len(bb)):
-    Y[len(bb)-1-k,:] = bb[k](tt)
+    Y[k,:] = bb[k](tt)
     
 
 X = np.zeros((len(bb), len(tt)))
@@ -98,7 +93,7 @@ class animTrailers:
         
     def initAnim(self):
         self.ax.clear()
-        self.ln, = self.ax.plot(self.TX[:,0], self.TY[:,0])
+        self.ln, = self.ax.plot(self.TX[:,-1], self.TY[:,-1])
         self.ax.set_aspect(aspect='equal', adjustable='box')
         self.ax.set_xlim(left=np.min(self.TX), right=np.max(self.TX))
         self.ax.set_ylim(bottom=np.min(self.TY), top=np.max(self.TY))
@@ -114,8 +109,16 @@ class animTrailers:
     def anim(self):
         return FuncAnimation(self.fig, self.updateTrailers, self.frames, init_func=self.initAnim, blit=False, repeat_delay=1000, interval=50)
         
-
-anim = animTrailers(TX, TY)
+nRep = 3
+TXX = np.concatenate((
+    np.matlib.repmat(TX[:,:1],1,nRep),
+    TX,
+    np.matlib.repmat(TX[:,-1:],1,nRep)), axis=1)
+TYY = np.concatenate((
+    np.matlib.repmat(TY[:,:1],1,nRep),
+    TY,
+    np.matlib.repmat(TY[:,-1:],1,nRep)), axis=1)
+anim = animTrailers(TXX, TYY)
 aa = anim.anim()
 
 # if saveAnimation:
